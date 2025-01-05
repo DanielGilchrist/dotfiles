d6d5b864906e101093332ec357027a8ac067254e
Daniel Gilchrist <daniel@workforce.com>
2025-01-05 18:26:45 +0000

    Simpler config for removing useless LSP hover notification

--- lua/plugins/noice.nvim.lua
+++ lua/plugins/noice.nvim.lua
@@ -1,20 +1,10 @@
-local mute_notification = function(text)
-  return {
-    filter = {
-      event = "notify",
-      find = text
-    },
-    opts = {
-      skip = true,
-    }
-  }
-end
-
 return {
   "folke/noice.nvim",
   opts = {
-    routes = {
-      mute_notification("No information available"),
+    lsp = {
+      hover = {
+        silent = true,
+      }
     }
   }
 }
