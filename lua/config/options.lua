eb6004f5f1d283d8335d7f18f1c7c2556bba1b78
Daniel Gilchrist <danniieelg@gmail.com>
2025-01-05 17:19:00 +0000

    Experiment with neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,17 @@
 
 -- vim.lsp.set_log_level("debug")
 
+if vim.g.neovide then
+  vim.o.guifont = "JetBrains Mono"
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_confirm_quit = true
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_cursor_animation_length = 0.05
+  vim.g.neovide_cursor_trail_size = 0.5
+  vim.g.neovide_cursor_smooth_blink = true
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+end
+
 vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "rubocop"
 
