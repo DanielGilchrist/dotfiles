2c30fd5f935c93fafea16deb08a45017b659c5ce
Daniel Gilchrist <daniel@workforce.com>
2025-01-07 16:09:55 +0000

    Add command for time worked

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -4,6 +4,24 @@ local function load(name)
   require("custom." .. name)
 end
 
+local function register_time_worked()
+  local function time_worked()
+    local notify = require("../utils/notify")
+    local time_worked_cmd = require("../utils/cmd").time_worked_cmd({ no_colour = true })
+
+    local function on_stdout(_, data)
+      notify.info(data)
+    end
+
+    vim.fn.jobstart(time_worked_cmd, {
+      stdout_buffered = true,
+      on_stdout = on_stdout,
+    })
+  end
+
+  vim.api.nvim_create_user_command("TimeWorked", time_worked, {})
+end
+
 return {
   lazy = true,
   event = { "CmdlineEnter" },
@@ -13,5 +31,7 @@ return {
     load("scratchpads")
     load("yank_test_line")
     load("bundle_open")
+
+    register_time_worked()
   end,
 }
--- lua/utils/cmd.lua
+++ lua/utils/cmd.lua
@@ -1,6 +1,13 @@
 return {
-  time_worked_cmd = function()
-    local command = "tanda_cli time_worked week 2>/dev/null"
+  time_worked_cmd = function(opts)
+    opts = opts == nil and {} or opts
+    local options_string = ""
+
+    if opts.no_colour then
+      options_string = options_string .. " --no-colour"
+    end
+
+    local command = string.format("tanda_cli time_worked week%s 2>/dev/null", options_string)
     local zsh_command = "zsh -ic '" .. command .. "'" -- zsh is a pain in the ass
     return command .. " || " .. zsh_command .. " || " .. "echo \"tanda_cli isn't setup!\""
   end
