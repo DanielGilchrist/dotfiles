2c6a612707e7acc626f26c88df0adc174e94c508
Daniel Gilchrist <danniieelg@gmail.com>
2025-01-21 13:47:48 +0000

    Remove <leader>M keymap

--- nvim/lua/plugins/treesj.lua
+++ nvim/lua/plugins/treesj.lua
@@ -1,10 +1,7 @@
 return {
   "Wansmer/treesj",
-  keys = { "<leader>M" },
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    require("treesj").setup({
-      use_default_keymaps = false
-    })
-  end,
+  opts = {
+    use_default_keymaps = false,
+  }
 }
