681970c6abb0bb578e8b3f497cdbd4c59d98252c
Daniel Gilchrist <danniieelg@gmail.com>
2025-02-07 14:06:03 +0000

    neovim: Add Ruby specific dials

--- nvim/lua/plugins/dial.nvim.lua
+++ nvim/lua/plugins/dial.nvim.lua
@@ -1,6 +1,8 @@
-local function register_default(opts, ...)
-  local args = { ... }
-  vim.list_extend(opts.groups.default, args)
+local function register_groups(opts, groups)
+  for key, value in pairs(groups) do
+    opts.groups[key] = opts.groups[key] or {}
+    vim.list_extend(opts.groups[key], value)
+  end
 end
 
 return {
@@ -20,10 +22,34 @@ return {
       cyclic = true,
     })
 
-    register_default(
+    local rails_model_associations = augend.constant.new({
+      elements = { "belongs_to", "has_one", "has_many" },
+      word = true,
+      cyclic = true,
+    })
+
+    local ruby_class_module = augend.constant.new({
+      elements = { "class", "module" },
+      word = true,
+      cyclic = true,
+    })
+
+    opts.dials_by_ft = vim.tbl_extend("force", opts.dials_by_ft, {
+      ruby = "ruby",
+    })
+
+    register_groups(
       opts,
-      sorbet_sigil,
-      http_verbs_lowercase
+      {
+        default = {
+          http_verbs_lowercase,
+        },
+        ruby = {
+          sorbet_sigil,
+          rails_model_associations,
+          ruby_class_module,
+        }
+      }
     )
   end
 }
