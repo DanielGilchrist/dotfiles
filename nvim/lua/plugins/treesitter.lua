692c7fc44014fa88ab8ea4cdac1304d64d2b6075
Daniel Gilchrist <danniieelg@gmail.com>
2025-02-07 16:47:24 +0000

    neovim: Simplify treesitter setup

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,21 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-        ensure_installed = {
-          "ruby",
-          "rust",
-          "go",
-          "lua",
-          "javascript",
-          "json",
-          "html",
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = false },
-        auto_install = false,
-        ignore_install = {},
-      })
-    end,
+    opts = {
+      ensure_installed = {
+        "ruby",
+        "rust",
+        "go",
+        "lua",
+        "javascript",
+        "json",
+        "html",
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = false },
+      auto_install = false,
+    },
   },
 }
