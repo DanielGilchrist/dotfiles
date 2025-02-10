da7cdd31d71c54b67c67bdbe9676f9387f2ea6c0
Daniel Gilchrist <danniieelg@gmail.com>
2025-02-10 14:48:49 +0000

    neovim: Add keymap for highlighting and replacing in current file with grug-far

---
+++ nvim/lua/plugins/grug-far.lua
@@ -0,0 +1,17 @@
+return {
+  "MagicDuck/grug-far.nvim",
+  keys = {
+    {
+      "<leader>cr",
+      function()
+        require('grug-far').with_visual_selection({
+          prefills = {
+            paths = vim.fn.expand("%"),
+          },
+        })
+      end,
+      mode = "v",
+      desc = "Search and replace (Current file)"
+    }
+  }
+}
