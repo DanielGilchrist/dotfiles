e988a7cea4e2823d78a5d435bd80b181d7c4c28f
Daniel Gilchrist <danniieelg@gmail.com>
2025-02-24 13:27:56 +0000

    neovim/wezterm: Try out everforest theme for a bit

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -20,7 +20,7 @@ local misc = {
   webgpu_power_preference = "HighPerformance",
   max_fps = 144,
 
-  color_scheme = "Jellybeans",
+  color_scheme = "Everforest Dark Hard (Gogh)",
   automatically_reload_config = false,
   window_close_confirmation = "AlwaysPrompt",
   notification_handling = "AlwaysShow",
