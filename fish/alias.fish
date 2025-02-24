79786606f034d249ea4a43e4750438a70c915587
Daniel Gilchrist <danniieelg@gmail.com>
2025-02-24 11:40:19 +0000

    fish: add lazygit alias as lg + reorganise aliases a bit

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,13 +1,13 @@
 set REPOS_DIR "$HOME/Documents/repos"
 set PERSONAL_DIR $REPOS_DIR/DanielGilchrist
 
-# whatever
 alias srbtc="bundle exec srb tc"
+alias lg=lazygit
+
+# personal projects
 alias tanda_cli="$PERSONAL_DIR/tanda_cli/bin/tanda_cli"
 alias tcli=tanda_cli
 alias poke_search="$PERSONAL_DIR/poke_search/target/release/poke_search"
 alias pks=poke_search
-
-# dev
-alias fug="fish $REPOS_DIR/DanielGilchrist/thefug/thefug"
-alias thefugbindev="$REPOS_DIR/DanielGilchrist/thefug/target/debug/thefugbindev"
+alias fug="fish /Users/danielgilchrist/Documents/repos/DanielGilchrist/thefug/thefug"
+alias fugd="/Users/danielgilchrist/Documents/repos/DanielGilchrist/thefug/target/debug/thefugbindev"
