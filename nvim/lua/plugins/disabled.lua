b623b925147120551142ad63c94351395e9f666a
Daniel Gilchrist <danniieelg@gmail.com>
2025-02-27 18:18:37 +0000

    neovim: Remove neotree
    Thought I'd already done this, have been using oil.nvim for a while now
    in its place

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -6,4 +6,5 @@ return {
   disable_plugin("mfussenegger/nvim-lint"),
   disable_plugin("stevearc/conform.nvim"),
   disable_plugin("rafamadriz/friendly-snippets"),
+  disable_plugin("nvim-neo-tree/neo-tree.nvim"), -- replaced by oil.nvim
 }
