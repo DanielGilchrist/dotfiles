9071c0617f8d5aac82d85c95e6cc881103694fa9
Daniel Gilchrist <danniieelg@gmail.com>
2025-03-03 11:00:11 +0000

    neovim: reconfigure snacks picker preview scroll keys

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -103,6 +103,18 @@ return {
       timeout = 5000,
       top_down = false,
     },
+    picker = {
+      win = {
+        input = {
+          keys = {
+            ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
+            ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
+            ["<c-f>"] = { "list_scroll_down", mode = { "i", "n" } },
+            ["<c-b>"] = { "list_scroll_up", mode = { "i", "n" } },
+          }
+        }
+      }
+    },
     terminal = {},
   },
   keys = {
