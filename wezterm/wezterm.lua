fec4dc35e10a9e173e7e8094c014801c1fcb8fd5
Daniel Gilchrist <danniieelg@gmail.com>
2025-03-21 10:16:34 +0000

    Switch to gruvbox

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -20,7 +20,7 @@ local misc = {
   webgpu_power_preference = "HighPerformance",
   max_fps = 144,
 
-  color_scheme = "Rosé Pine (Gogh)",
+  color_scheme = "Gruvbox dark, hard (base16)",
   automatically_reload_config = false,
   window_close_confirmation = "AlwaysPrompt",
   notification_handling = "AlwaysShow",
