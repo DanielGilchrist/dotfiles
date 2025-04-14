1b18b95ea7af883a590dc31ab766ca22f02a8783
Daniel Gilchrist <danniieelg@gmail.com>
2025-04-14 11:56:00 +0100

    neovim: Drop codecompanion, try out avante

---
+++ nvim/lua/plugins/avante.nvim.lua
@@ -0,0 +1,31 @@
+return {
+  "yetone/avante.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = { file_types = { "markdown", "Avante" } },
+      ft = { "markdown", "Avante" },
+    },
+  },
+  build = "make",
+  opts = {
+    provider = "claude",
+    auto_suggestions_provider = "claude",
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-3-7-sonnet-latest",
+      temperature = 0,
+      max_tokens = 16000
+    },
+    behaviour = {
+      auto_suggestions = true,
+      enable_claude_text_editor_tool_mode = true,
+    },
+    ask = {
+      floating = true
+    }
+  },
+}
