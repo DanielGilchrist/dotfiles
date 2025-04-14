44a8309fd9f00d4e9aced267e31abda9c1a31cdc
Daniel Gilchrist <danniieelg@gmail.com>
2025-04-14 11:56:00 +0100

    neovim: Drop codecompanion, try out avante

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,5 @@
+return {
+  "zbirenbaum/copilot.lua",
+  event = { "CmdlineEnter", "InsertEnter" },
+  opts = {},
+}
--- nvim/lua/plugins/markview.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "OXY2DEV/markview.nvim",
-  event = "Bufread *.md",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons"
-  }
-}
