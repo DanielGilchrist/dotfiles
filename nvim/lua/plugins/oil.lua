7df80c53f71c91b6c808c3b28046c3ccd0ccb1b2
Daniel Gilchrist <danniieelg@gmail.com>
2025-06-16 13:56:04 +0100

    neovim: Show preview when opening up oil buffer

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -12,6 +12,9 @@ return {
     },
     float = {
       padding = 5,
+      preview = {
+        vertical = true
+      }
     },
     view_options = {
       show_hidden = true,
@@ -21,7 +24,27 @@ return {
     {
       "<leader>e",
       function()
-        require("oil").toggle_float()
+        -- TODO: Look into allowing toggle_float to accept opts just like open_float
+        -- API Docs: https://github.com/stevearc/oil.nvim/blob/master/doc/api.md#open_floatdir-opts-cb
+        -- toggle_float def: https://github.com/stevearc/oil.nvim/blob/08c2bce8b00fd780fb7999dbffdf7cd174e896fb/lua/oil/init.lua#L345-L351
+        --
+        -- require("oil").toggle_float(nil, {
+        --   preview = {
+        --     vertical = true
+        --   }
+        -- })
+
+        local oil = require("oil")
+
+        if vim.w.is_oil_win then
+          return oil.close()
+        end
+
+        oil.open_float(nil, {
+          preview = {
+            vertical = true
+          }
+        })
       end,
       desc = "Explore Files (Oil)",
     }
