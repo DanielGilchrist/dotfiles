4bcdbc5db8b6ac9782970363f07150ae9af5b4a9
Daniel Gilchrist <danniieelg@gmail.com>
2025-07-03 10:04:08 +0100

    neovim: Enable word wrap for markdown files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -26,3 +26,11 @@ create_autocmd("InsertLeave", {
     vim.opt.relativenumber = true
   end
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
