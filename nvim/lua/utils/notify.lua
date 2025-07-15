9860a6c51b1e14dcb02a89c4213cf3d76e3cc307
Daniel Gilchrist <danniieelg@gmail.com>
2025-07-15 13:53:39 +0100

    neovim: Add types/docs for certain APIs

--- nvim/lua/utils/notify.lua
+++ nvim/lua/utils/notify.lua
@@ -6,17 +6,33 @@ local function merge_defaults(title, opts)
   })
 end
 
-return {
-  info = function(msg, opts)
-    Snacks.notify.info(msg, merge_defaults("Information", opts))
-  end,
-  warn = function(msg, opts)
-    Snacks.notify.warn(msg, merge_defaults("Warning", opts))
-  end,
-  error = function(msg, opts)
-    Snacks.notify.error(msg, merge_defaults("Error", opts))
-  end,
-  hide = function(id)
-    Snacks.notifier.hide(id)
-  end
-}
+local M = {}
+
+---Notify of information
+---@param msg string
+---@param opts? snacks.notify.Opts
+M.info = function(msg, opts)
+  Snacks.notify.info(msg, merge_defaults("Information", opts))
+end
+
+---Notify of a warning
+---@param msg string
+---@param opts? snacks.notify.Opts
+M.warn = function(msg, opts)
+  Snacks.notify.warn(msg, merge_defaults("Warning", opts))
+end
+
+---Notify of an error
+---@param msg string
+---@param opts? snacks.notify.Opts
+M.error = function(msg, opts)
+  Snacks.notify.error(msg, merge_defaults("Error", opts))
+end
+
+---Hide notification popup
+---@param id number|string
+M.hide = function(id)
+  Snacks.notifier.hide(id)
+end
+
+return M
--- nvim/lua/utils/path.lua
+++ nvim/lua/utils/path.lua
@@ -2,6 +2,8 @@ local lua_path = vim.fn.stdpath("config") .. "/lua"
 
 return {
   lua_path = lua_path,
+  ---@param relative_path string
+  ---@return string
   absolute_path = function(relative_path)
     return lua_path .. relative_path
   end
