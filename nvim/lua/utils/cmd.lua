9ca6ee71af995681ddb5a68140c4ece9c7c8e86b
Daniel Gilchrist <danniieelg@gmail.com>
2025-08-17 12:38:37 +0100

    Handle stderr for tanda_cli commands

--- nvim/lua/utils/cmd.lua
+++ nvim/lua/utils/cmd.lua
@@ -11,22 +11,24 @@ M = {}
 
 ---@class TandaCliOpts
 ---@field on_stdout? fun(job_id: integer, data: string[], name: string) Callback for stdout
+---@field on_stderr? fun(job_id: integer, data: string[], name: string) Callback for stderr
 
 ---@param opts? TandaCliOpts Optional configuration
----@return string? # Returns command string if no on_stdout provided
+---@return string? # Returns command string if no `on_stdout` or `on_stderr` provided
 M.tanda_cli = function(args, opts)
   opts = opts == nil and {} or opts
   ---@cast opts -nil
 
   local command = build_tanda_cli_cmd(args)
 
-  if not opts.on_stdout then
+  if not opts.on_stdout or not opts.on_stderr then
     return command
   end
 
   vim.fn.jobstart(command, {
     stdout_buffered = true,
     on_stdout = opts.on_stdout,
+    on_stderr = opts.on_stderr,
   })
 end
 
