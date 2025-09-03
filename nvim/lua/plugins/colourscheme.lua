2a6efe7d90d967edf5f07c91a11a077b708aabaf
Daniel Gilchrist <danniieelg@gmail.com>
2025-09-03 22:43:45 +0100

    Update theme to "evergarden"

--- nvim/lua/plugins/colourscheme.lua
+++ nvim/lua/plugins/colourscheme.lua
@@ -1,4 +1,4 @@
-transparent = false
+local transparent = false
 
 local function colourscheme(source, options)
   options = options == nil and {} or options
@@ -15,10 +15,32 @@ end
 
 return {
   colourscheme("RRethy/base16-nvim"),
+  colourscheme("everviolet/nvim", {
+    name = "evergarden",
+    opts = {
+      theme = {
+        variant = "winter", -- "winter"|"fall"|"spring"|"summer"
+        -- https://github.com/everviolet/nvim/blob/4041ce92cf1387f19a8006c1e36969bfb5371c50/lua/evergarden/colors.lua#L6-L19
+        accent = "green",
+      },
+      editor = {
+        transparent_background = transparent,
+        sign = { color = "none" },
+        float = {
+          color = "mantle",
+          solid_border = false,
+        },
+        completion = {
+          color = "surface0",
+        },
+      },
+
+    }
+  }),
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "base16-gruvbox-material-dark-hard",
+      colorscheme = "evergarden",
     },
   },
 }
