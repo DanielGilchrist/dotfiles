8f2ca46822100617b56e7da0ea61342985d2b77f
Daniel Gilchrist <daniel@workforce.com>
2025-09-30 10:45:05 +0100

    wezterm: format lua files

--- wezterm/commands.lua
+++ wezterm/commands.lua
@@ -9,8 +9,8 @@ local M = {}
 
 ---@type PaneDirection
 local pane_direction = {
-  Right = "Right",
-  Bottom = "Bottom"
+	Right = "Right",
+	Bottom = "Bottom",
 }
 
 ---@class (exact) Commands
@@ -24,13 +24,13 @@ local pane_direction = {
 
 ---@type Commands
 local commands = {
-  CDT     = "cdt",
-  Console = "bin/dev console",
-  Server  = "bin/dev server",
-  Start   = "bin/dev start",
-  Tunnel  = "bin/tunnel",
-  Worker  = "bin/dev worker",
-  Webpack = "bin/dev webpack"
+	CDT = "cdt",
+	Console = "bin/dev console",
+	Server = "bin/dev server",
+	Start = "bin/dev start",
+	Tunnel = "bin/tunnel",
+	Worker = "bin/dev worker",
+	Webpack = "bin/dev webpack",
 }
 
 ---@class (exact) Region
@@ -39,159 +39,159 @@ local commands = {
 
 ---@type Region
 local regions = {
-  APAC = "apac",
-  EU   = "eu"
+	APAC = "apac",
+	EU = "eu",
 }
 
 local function split_pane_with(func)
-  return function(pane, direction, size)
-    size = size == nil and 0.5 or size
-    local new_pane = pane:split { direction = direction, size = size }
-    func(new_pane)
-    return new_pane
-  end
+	return function(pane, direction, size)
+		size = size == nil and 0.5 or size
+		local new_pane = pane:split({ direction = direction, size = size })
+		func(new_pane)
+		return new_pane
+	end
 end
 
 local function has_text(pane, ...)
-  local text = pane:get_logical_lines_as_text(pane:get_dimensions().scrollback_rows)
+	local text = pane:get_logical_lines_as_text(pane:get_dimensions().scrollback_rows)
 
-  for i = 1, select("#", ...) do
-    local pattern = select(i, ...)
+	for i = 1, select("#", ...) do
+		local pattern = select(i, ...)
 
-    if string.find(text, pattern) then
-      return true
-    end
-  end
+		if string.find(text, pattern) then
+			return true
+		end
+	end
 
-  return false
+	return false
 end
 
 local function run_command(pane, command)
-  pane:send_text(command .. "\n")
+	pane:send_text(command .. "\n")
 end
 
 local function wait_for_text_for(pane, ...)
-  while true do
-    if has_text(pane, ...) then
-      break
-    else
-      wezterm.sleep_ms(1000)
-    end
-  end
+	while true do
+		if has_text(pane, ...) then
+			break
+		else
+			wezterm.sleep_ms(1000)
+		end
+	end
 end
 
 local function notify(window, title, content)
-  -- idk why but I can't get `window:toast_notification` to work on mac for the life of me
-  if os_utils.system() == "macos" then
-    local q = "\""
-    local command = "display notification " .. q .. content .. q .. " with title " .. q .. title .. q
-    wezterm.run_child_process { "osascript", "-e", command }
-  else
-    window:toast_notification(title, content, nil, 4000)
-  end
+	-- idk why but I can't get `window:toast_notification` to work on mac for the life of me
+	if os_utils.system() == "macos" then
+		local q = '"'
+		local command = "display notification " .. q .. content .. q .. " with title " .. q .. title .. q
+		wezterm.run_child_process({ "osascript", "-e", command })
+	else
+		window:toast_notification(title, content, nil, 4000)
+	end
 end
 
 local function clear(pane)
-  pane:send_text('\x0c')
+	pane:send_text("\x0c")
 end
 
 local function has_interrupted_system_call(pane)
-  return has_text(pane, "cd: Interrupted system call")
+	return has_text(pane, "cd: Interrupted system call")
 end
 
 local function buffered_cdt(pane)
-  pane:send_text(commands.CDT)
-  wezterm.sleep_ms(100)
-  pane:send_text("\n")
+	pane:send_text(commands.CDT)
+	wezterm.sleep_ms(100)
+	pane:send_text("\n")
 end
 
 local function wait_for_text()
-  wezterm.sleep_ms(100)
+	wezterm.sleep_ms(100)
 end
 
 local function handle_potential_interrupted_system_call_from_cdt(pane, already_tried)
-  if has_interrupted_system_call(pane) then
-    if already_tried then
-      notify(pane.window, "OpenWorkTabs", "Error! Can't continue due to issue with executing `cdt`")
-      return true
-    else
-      clear(pane)
-      buffered_cdt(pane)
-      wait_for_text()
-      return handle_potential_interrupted_system_call_from_cdt(pane, true)
-    end
-  else
-    return
-  end
+	if has_interrupted_system_call(pane) then
+		if already_tried then
+			notify(pane.window, "OpenWorkTabs", "Error! Can't continue due to issue with executing `cdt`")
+			return true
+		else
+			clear(pane)
+			buffered_cdt(pane)
+			wait_for_text()
+			return handle_potential_interrupted_system_call_from_cdt(pane, true)
+		end
+	else
+		return
+	end
 end
 
 local function handle_missing_cdt_command(pane)
-  if has_text(pane, "Unknown command") then
-    notify(pane.window, "OpenWorkTabs", "Error! Please set a `cdt` alias")
-    return true
-  end
+	if has_text(pane, "Unknown command") then
+		notify(pane.window, "OpenWorkTabs", "Error! Please set a `cdt` alias")
+		return true
+	end
 end
 
 local function open_work_tabs(region)
-  return function(original_window, _original_pane, _line)
-    local function export_region_and_cdt(pane)
-      run_command(pane, "export REGION=" .. region)
-      run_command(pane, commands.CDT)
-    end
+	return function(original_window, _original_pane, _line)
+		local function export_region_and_cdt(pane)
+			run_command(pane, "export REGION=" .. region)
+			run_command(pane, commands.CDT)
+		end
 
-    local split_pane_with_setup = split_pane_with(export_region_and_cdt)
+		local split_pane_with_setup = split_pane_with(export_region_and_cdt)
 
-    local _tab, server_pane, window = original_window:mux_window():spawn_tab({})
+		local _tab, server_pane, window = original_window:mux_window():spawn_tab({})
 
-    local gui_window = window:gui_window()
-    gui_window:perform_action(wezterm.action.MoveTab(0), server_pane)
+		local gui_window = window:gui_window()
+		gui_window:perform_action(wezterm.action.MoveTab(0), server_pane)
 
-    export_region_and_cdt(server_pane)
-    wait_for_text()
+		export_region_and_cdt(server_pane)
+		wait_for_text()
 
-    if handle_missing_cdt_command(server_pane) then
-      return
-    end
+		if handle_missing_cdt_command(server_pane) then
+			return
+		end
 
-    if handle_potential_interrupted_system_call_from_cdt(server_pane) then
-      return
-    end
+		if handle_potential_interrupted_system_call_from_cdt(server_pane) then
+			return
+		end
 
-    local console_pane = split_pane_with_setup(server_pane, pane_direction.Right)
-    local webpack_pane = split_pane_with_setup(console_pane, pane_direction.Bottom, 0.1)
-    local tunnel_pane = split_pane_with_setup(server_pane, pane_direction.Bottom, 0.1)
-    local worker_pane = split_pane_with_setup(server_pane, pane_direction.Bottom, 0.4)
+		local console_pane = split_pane_with_setup(server_pane, pane_direction.Right)
+		local webpack_pane = split_pane_with_setup(console_pane, pane_direction.Bottom, 0.1)
+		local tunnel_pane = split_pane_with_setup(server_pane, pane_direction.Bottom, 0.1)
+		local worker_pane = split_pane_with_setup(server_pane, pane_direction.Bottom, 0.4)
 
-    if region == regions.EU then
-      run_command(tunnel_pane, commands.Start)
-      wait_for_text_for(tunnel_pane, "Your dev box", "is ready to be used")
-    end
+		if region == regions.EU then
+			run_command(tunnel_pane, commands.Start)
+			wait_for_text_for(tunnel_pane, "Your dev box", "is ready to be used")
+		end
 
-    run_command(tunnel_pane, commands.Tunnel)
-    wait_for_text_for(tunnel_pane, "INFO: Ready!")
+		run_command(tunnel_pane, commands.Tunnel)
+		wait_for_text_for(tunnel_pane, "INFO: Ready!")
 
-    run_command(server_pane, commands.Server)
-    wait_for_text_for(server_pane, "Done in", "Running server at")
+		run_command(server_pane, commands.Server)
+		wait_for_text_for(server_pane, "Done in", "Running server at")
 
-    run_command(webpack_pane, commands.Webpack)
-    run_command(console_pane, commands.Console)
-    run_command(worker_pane, commands.Worker)
-  end
+		run_command(webpack_pane, commands.Webpack)
+		run_command(console_pane, commands.Console)
+		run_command(worker_pane, commands.Worker)
+	end
 end
 
 M.register_commands = function()
-  wezterm.on("augment-command-palette", function(_window, _pane)
-    return {
-      {
-        brief = "[APAC] Open work tabs",
-        action = wezterm.action_callback(open_work_tabs(regions.APAC))
-      },
-      {
-        brief = "[EU] Open work tabs",
-        action = wezterm.action_callback(open_work_tabs(regions.EU))
-      },
-    }
-  end)
+	wezterm.on("augment-command-palette", function(_window, _pane)
+		return {
+			{
+				brief = "[APAC] Open work tabs",
+				action = wezterm.action_callback(open_work_tabs(regions.APAC)),
+			},
+			{
+				brief = "[EU] Open work tabs",
+				action = wezterm.action_callback(open_work_tabs(regions.EU)),
+			},
+		}
+	end)
 end
 
 return M
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,97 +9,97 @@ local table_utils = require("utils.table")
 local command = key_utils.command_key()
 
 wezterm.on("gui-startup", function(cmd)
-  local _tab, _pane, window = mux.spawn_window(cmd or {})
-  local gui_window = window:gui_window()
+	local _tab, _pane, window = mux.spawn_window(cmd or {})
+	local gui_window = window:gui_window()
 
-  gui_window:toggle_fullscreen()
+	gui_window:toggle_fullscreen()
 end)
 
 local misc = {
-  front_end = "WebGpu",
-  webgpu_power_preference = "HighPerformance",
-  max_fps = 144,
+	front_end = "WebGpu",
+	webgpu_power_preference = "HighPerformance",
+	max_fps = 144,
 
-  color_scheme = "Catppuccin Mocha",
-  colors = {
-    background = "#1E2528",
-    foreground = "#F8F9E8",
-    scrollbar_thumb = "#58686D",
-    split = "#CBE3B3",
+	color_scheme = "Catppuccin Mocha",
+	colors = {
+		background = "#1E2528",
+		foreground = "#F8F9E8",
+		scrollbar_thumb = "#58686D",
+		split = "#CBE3B3",
 
-    tab_bar = {
-      background = "#191E21",
-      active_tab = {
-        bg_color = "#CBE3B3",
-        fg_color = "#171C1F",
-      },
-      new_tab = {
-        bg_color = "#191E21",
-        fg_color = "#CBE3B3",
-      },
-      new_tab_hover = {
-        bg_color = "#262F33",
-        fg_color = "#CBE3B3",
-      },
-      inactive_tab = {
-        bg_color = "#191E21",
-        fg_color = "#6F8788",
-      },
-      inactive_tab_hover = {
-        bg_color = "#262F33",
-        fg_color = "#ADC9BC",
-      },
-    },
-  },
+		tab_bar = {
+			background = "#191E21",
+			active_tab = {
+				bg_color = "#CBE3B3",
+				fg_color = "#171C1F",
+			},
+			new_tab = {
+				bg_color = "#191E21",
+				fg_color = "#CBE3B3",
+			},
+			new_tab_hover = {
+				bg_color = "#262F33",
+				fg_color = "#CBE3B3",
+			},
+			inactive_tab = {
+				bg_color = "#191E21",
+				fg_color = "#6F8788",
+			},
+			inactive_tab_hover = {
+				bg_color = "#262F33",
+				fg_color = "#ADC9BC",
+			},
+		},
+	},
 
-  automatically_reload_config = false,
-  window_close_confirmation = "AlwaysPrompt",
-  notification_handling = "AlwaysShow",
-  exit_behavior = "Close",
+	automatically_reload_config = false,
+	window_close_confirmation = "AlwaysPrompt",
+	notification_handling = "AlwaysShow",
+	exit_behavior = "Close",
 
-  enable_scroll_bar = true, -- per pane scrollbar 👀  - https://github.com/wez/wezterm/pull/1886
-  scrollback_lines = 50000,
+	enable_scroll_bar = true, -- per pane scrollbar 👀  - https://github.com/wez/wezterm/pull/1886
+	scrollback_lines = 50000,
 
-  window_padding = {
-    left = 0,
-    right = 10, -- controls the width of the scrollbar
-    top = 10,
-    bottom = 0,
-  },
-  native_macos_fullscreen_mode = true,
-  -- window_background_opacity = 0.90,
-  -- macos_window_background_blur = 20,
+	window_padding = {
+		left = 0,
+		right = 10, -- controls the width of the scrollbar
+		top = 10,
+		bottom = 0,
+	},
+	native_macos_fullscreen_mode = true,
+	-- window_background_opacity = 0.90,
+	-- macos_window_background_blur = 20,
 
-  font = wezterm.font_with_fallback({
-    {
-      family = "JetBrains Mono NL",
-      harfbuzz_features = { "liga=1" },
-    },
-  }),
-  font_size = os_utils.system() == "macos" and 14 or 12,
+	font = wezterm.font_with_fallback({
+		{
+			family = "JetBrains Mono NL",
+			harfbuzz_features = { "liga=1" },
+		},
+	}),
+	font_size = os_utils.system() == "macos" and 14 or 12,
 
-  mouse_bindings = {
-    -- CMD + click links
-    {
-      event = { Up = { streak = 1, button = "Left" } },
-      mods = command,
-      action = wezterm.action.OpenLinkAtMouseCursor,
-    },
-  },
+	mouse_bindings = {
+		-- CMD + click links
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = command,
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
 }
 
 local new_tab_icon = " + "
 local tabs = {
-  enable_tab_bar = true,
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
+	enable_tab_bar = true,
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
 
-  tab_bar_style = {
-    new_tab = new_tab_icon,
-    new_tab_hover = new_tab_icon,
-  },
+	tab_bar_style = {
+		new_tab = new_tab_icon,
+		new_tab_hover = new_tab_icon,
+	},
 
-  tab_max_width = 25,
+	tab_max_width = 25,
 }
 
 return table_utils.merge_all(misc, require("key_bindings"), tabs, {})
