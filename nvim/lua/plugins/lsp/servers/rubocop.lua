eb08c4c27277251bd94e26cdfb66832c2988b3f1
Daniel Gilchrist <daniel@workforce.com>
2025-10-10 09:12:34 +0100

    neovim: Enable YJIT with rubocop LSP

--- nvim/lua/plugins/lsp/servers/rubocop.lua
+++ nvim/lua/plugins/lsp/servers/rubocop.lua
@@ -2,6 +2,6 @@ return {
   server = {
     single_file_support = false,
     mason = false,
-    cmd = { "bundle", "exec", "rubocop", "--lsp" },
-  }
+    cmd = { "env", "RUBY_YJIT_ENABLE=1", "bundle", "exec", "rubocop", "--lsp" },
+  },
 }
