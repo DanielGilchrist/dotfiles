563151a7360ce372c1fa772bcf785d0ae732d972
Daniel Gilchrist <danniieelg@gmail.com>
2025-10-17 15:35:12 +0100

    neovim: override lazyvim window resize maps

--- nvim/lua/plugins/lazy_vim.lua
+++ nvim/lua/plugins/lazy_vim.lua
@@ -2,4 +2,15 @@ return {
   "LazyVim/LazyVim",
   commit = "144fe24be15007de0db036ba145514011d4d55d6",
   -- version = "15.*",
+  keys = {
+    { "<C-s",      false },
+    { "<C-Up>",    false },
+    { "<C-Down>",  false },
+    { "<C-Left>",  false },
+    { "<C-Right>", false },
+    { "<A-Up>",    "<cmd>resize +2<cr>",          desc = "Increase Window Height" },
+    { "<A-Down>",  "<cmd>resize -2<cr>",          desc = "Decrease Window Height" },
+    { "<A-Left>",  "<cmd>vertical resize -2<cr>", desc = "Decrease Window Width" },
+    { "<A-Right>", "<cmd>vertical resize +2<cr>", desc = "Increase Window Width" },
+  },
 }
