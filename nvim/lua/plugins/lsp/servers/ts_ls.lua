9dff572a8e1afef343507d65a330a376f25100f2
Daniel Gilchrist <daniel@workforce.com>
2026-01-22 14:03:48 +0000

    neovim: Stop ts_ls from starting in projects using flowjs

--- nvim/lua/plugins/lsp/servers/ts_ls.lua
+++ nvim/lua/plugins/lsp/servers/ts_ls.lua
@@ -1,1 +1,32 @@
-return {}
+-- Copied from https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ts_ls.lua
+local default_root_dir = function(bufnr, on_dir)
+  -- The project root is where the LSP can be started from
+  -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+  -- We select then from the project root, which is identified by the presence of a package
+  -- manager lock file.
+  local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+  -- Give the root markers equal priority by wrapping them in a table
+  root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
+      or vim.list_extend(root_markers, { '.git' })
+  -- exclude deno
+  local deno_path = vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc', 'deno.lock' })
+  local project_root = vim.fs.root(bufnr, root_markers)
+  if deno_path and (not project_root or #deno_path >= #project_root) then
+    return
+  end
+  -- We fallback to the current working directory if no project root is found
+  on_dir(project_root or vim.fn.getcwd())
+end
+
+return {
+  server = {
+    root_dir = function(bufnr, on_dir)
+      local flow_path = vim.fs.root(bufnr, { ".flowconfig" })
+      if flow_path then
+        return
+      end
+
+      default_root_dir(bufnr, on_dir)
+    end,
+  }
+}
