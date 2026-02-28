dc0f9ab56d78c5782457ecae17f7403fcbb1a46e
Daniel Gilchrist <danniieelg@gmail.com>
2026-02-28 02:35:43 +0000

    Delete JAVA_HOME for now

--- fish/config/macos.fish
+++ fish/config/macos.fish
@@ -4,7 +4,6 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 alias nproc="sysctl -n hw.logicalcpu"
 
 set -gx HOMEBREW_NO_AUTO_UPDATE 1
-set -gx JAVA_HOME "/Applications/Android Studio.app/Contents/jbr/Contents/Home"
 set -gx ANDROID_HOME $HOME/Library/Android/sdk
 
 set -gx PATH $PATH $ANDROID_HOME/emulator
