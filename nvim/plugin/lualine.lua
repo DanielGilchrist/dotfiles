7ec8c28339fc759c94cf2b60c30f2b43d09c5dc7
Daniel Gilchrist <daniel@workforce.com>
2026-04-01 15:27:41 +0100

    Configure lualine to only show buffers + remove alternate_file indicator

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -3,6 +3,11 @@ local pack = require("utils.pack")
 pack.add({ "https://github.com/nvim-lualine/lualine.nvim" })
 
 require("lualine").setup({
+  tabline = {
+    lualine_a = {},
+    lualine_b = { { "buffers", symbols = { alternate_file = "" } } },
+    lualine_z = {},
+  },
   extensions = {
     "oil",
   },
