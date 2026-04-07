55a647c87c5ed22661adb18c076e64747ca4e580
Daniel Gilchrist <daniel@workforce.com>
2026-04-07 14:39:59 +0100

    Add wezterm types + fix dep warning

--- nvim/plugin/lazydev.lua
+++ nvim/plugin/lazydev.lua
@@ -1,9 +1,13 @@
 local pack = require("utils.pack")
 
-pack.later({ "https://github.com/folke/lazydev.nvim" }, function()
+pack.later({
+  "https://github.com/folke/lazydev.nvim",
+  "https://github.com/DrKJeff16/wezterm-types",
+}, function()
   require("lazydev").setup({
     library = {
       { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      { path = "wezterm-types", words = { "wezterm" } },
       "snacks.nvim",
     },
   })
