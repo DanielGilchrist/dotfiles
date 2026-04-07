a6203fe85c055f5a6daf1b632b7fdbf792316eda
Daniel Gilchrist <daniel@workforce.com>
2026-04-07 11:42:59 +0100

    neovim: Simplify tabline separators

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -3,6 +3,10 @@ local pack = require("utils.pack")
 pack.add({ "https://github.com/nvim-lualine/lualine.nvim" })
 
 require("lualine").setup({
+  options = {
+    section_separators = { left = "", right = "" },
+    component_separators = { left = "", right = "" },
+  },
   tabline = {
     lualine_a = {},
     lualine_b = { { "buffers", symbols = { alternate_file = "" } } },
