23d292702bdd81f322be65e6de6c775f56430633
Daniel Gilchrist <danniieelg@gmail.com>
2026-04-15 12:03:02 +0100

    neovim: remove vim-haml and use treesitter for HAML

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -9,14 +9,36 @@ return {
       vim.opt.rtp:append(ts_path)
     end
 
-    require("nvim-treesitter.parsers").crystal = {
-      install_info = {
-        url = "https://github.com/crystal-lang-tools/tree-sitter-crystal",
-        generate = false,
-        generate_from_json = false,
-        queries = "queries/nvim",
+    local custom_parsers = {
+      crystal = {
+        install_info = {
+          url = "https://github.com/crystal-lang-tools/tree-sitter-crystal",
+          revision = "50ca9e6fcfb16a2cbcad59203cfd8ad650e25c49",
+          queries = "queries/nvim",
+        },
+      },
+      haml = {
+        install_info = {
+          url = "https://github.com/vitallium/tree-sitter-haml",
+          revision = "3ea15266a86dc4d921e8a2c2213d1ca15661d7ba",
+          queries = "queries",
+        },
       },
     }
+
+    local function register_custom_parsers()
+      local parsers = require("nvim-treesitter.parsers")
+      for name, config in pairs(custom_parsers) do
+        parsers[name] = config
+      end
+    end
+
+    register_custom_parsers()
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "TSUpdate",
+      callback = register_custom_parsers,
+    })
 
     vim.api.nvim_create_autocmd("FileType", {
       callback = function()
--- nvim/lua/plugins/vim_haml.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "tpope/vim-haml",
-  ft = { "haml", "sass", "scss" },
-}
