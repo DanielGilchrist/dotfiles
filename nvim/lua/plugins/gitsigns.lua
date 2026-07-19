079e9c4d4b71126598f7569f44f9b16cfac0eecf
Daniel Gilchrist <danniieelg@gmail.com>
2026-07-19 14:16:51 +0100

    neovim: Delete unused gitsigns keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,34 +3,5 @@ return {
   event = "BufReadPost",
   opts = {
     current_line_blame = true,
-    on_attach = function(bufnr)
-      local gs = require("gitsigns")
-      local function map(mode, l, r, desc)
-        vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc, silent = true })
-      end
-
-      map("n", "]h", function()
-        if vim.wo.diff then
-          vim.cmd.normal({ "]c", bang = true })
-        else
-          gs.nav_hunk("next")
-        end
-      end, "Next Hunk")
-
-      map("n", "[h", function()
-        if vim.wo.diff then
-          vim.cmd.normal({ "[c", bang = true })
-        else
-          gs.nav_hunk("prev")
-        end
-      end, "Prev Hunk")
-
-      map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
-      map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
-      map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
-      map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
-
-      require("utils.which_key").refresh_triggers(bufnr)
-    end,
   },
 }
