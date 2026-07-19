eda65510f666bf80a5a918acd0b2ad9767f11f49
Daniel Gilchrist <danniieelg@gmail.com>
2026-07-19 14:17:12 +0100

    neovim: Fix unified bug causing crash during review

--- nvim/lua/plugins/unified.lua
+++ nvim/lua/plugins/unified.lua
@@ -4,4 +4,22 @@ return {
   opts = {
     file_tree = { enabled = false },
   },
+  config = function(_, opts)
+    require("unified").setup(opts)
+    local async = require("unified.utils.async")
+    async.debounce = function(func, delay_ms)
+      local timer
+      return function(...)
+        local args = { ... }
+        if timer and not timer:is_closing() then
+          timer:stop()
+          timer:close()
+        end
+        timer = vim.defer_fn(function()
+          timer = nil
+          func(unpack(args))
+        end, delay_ms)
+      end
+    end
+  end,
 }
