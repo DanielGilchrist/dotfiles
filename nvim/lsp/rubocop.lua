50864978417433c69ccd325daf7dd30c07732bb2
Daniel Gilchrist <danniieelg@gmail.com>
2026-09-11 09:06:46 +0100

    lsp: Fix root markers for rubocop/sorbet

--- nvim/lsp/rubocop.lua
+++ nvim/lsp/rubocop.lua
@@ -1,6 +1,6 @@
 return {
   cmd = { "env", "RUBY_YJIT_ENABLE=1", "bundle", "exec", "rubocop", "--lsp" },
   filetypes = { "ruby" },
-  root_markers = { "Gemfile", ".git" },
+  root_markers = { ".rubocop.yml", ".rubocop_todo.yml" },
   workspace_required = true,
 }
--- nvim/lsp/sorbet.lua
+++ nvim/lsp/sorbet.lua
@@ -1,6 +1,15 @@
 return {
   cmd = { "bundle", "exec", "srb", "tc", "--lsp" },
   filetypes = { "ruby" },
-  root_markers = { "Gemfile", ".git" },
   workspace_required = true,
+  root_dir = function(bufnr, on_dir)
+    local start = vim.api.nvim_buf_get_name(bufnr)
+    if start == "" then start = vim.fn.getcwd() end
+    for dir in vim.fs.parents(start) do
+      if vim.uv.fs_stat(dir .. "/sorbet/config") then
+        on_dir(dir)
+        return
+      end
+    end
+  end,
 }
